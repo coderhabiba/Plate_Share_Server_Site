c380d216a20cd4d7db0b9c94a9bdc137f7a4aba2
coderhabiba <anikahabiba50@gmail.com>
2025-11-09 14:32:38 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -11,6 +11,42 @@ dotenv.config();
 app.use(corse());
 app.use(express.json());
 
+// mongodb uri
+const uri ='mongodb+srv://<db_username>:<db_password>@cluster0.sugbz4l.mongodb.net/?appName=Cluster0';
+
+// MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	
+    await client.connect();
+    
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // 
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
